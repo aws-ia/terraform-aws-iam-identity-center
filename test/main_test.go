34317289878188c49f8e7028c876659e38473ae3
Kevon Mayers <novekm@amazon.com>
2024-02-21 02:20:09 -0500

    update test, remove go.sh

--- test/main_test.go
+++ test/main_test.go
@@ -1,7 +1,6 @@
 package test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
@@ -15,19 +14,10 @@ func TestAWSandCustomerManagedPolicies(t *testing.T) {
 
 	}
 
-	// At the end of the test, run 'terrform destroy' to clean up any resources that were created
-	defer terraform.DestroyE(t, terraformOptions)
+	// At the end of the test, run 'terraform destroy' to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
 
 	// Run 'terraform init' and 'terraform apply'. Fail the test if there are any errors.
-	terraform.InitAndApplyE(t, terraformOptions)
-
-	if _, err := terraform.ApplyE(t, terraformOptions); err != nil {
-  // Do something with err
-		fmt.Print("Expected potential error - Skip")
-	}
-	if _, err := terraform.DestroyE(t, terraformOptions); err != nil {
-  // Do something with err
-	fmt.Print("Expected potential error - Skip")
-	}
+	terraform.InitAndApply(t, terraformOptions)
 
 }
