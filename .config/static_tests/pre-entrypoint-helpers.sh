17cb6b4b4c0259ede0df36c80864c4d314d25b89
IA Automator <dev@null>
2024-04-09 16:19:00 +0000

    (automated) Updates from project type

---
+++ .config/static_tests/pre-entrypoint-helpers.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+## NOTE: this script runs at the start of static test
+## use this to load any configuration before the static test 
+## TIPS: avoid modifying the .project_automation/static_test/entrypoint.sh
+## migrate any customization you did on entrypoint.sh to this helper script
+echo "Executing Pre-Entrypoint Helpers"
--- .project_automation/static_tests/entrypoint.sh
+++ .project_automation/static_tests/entrypoint.sh
@@ -1,63 +1,50 @@
-#!/bin/bash -e
+#!/bin/bash
+
+## WARNING: DO NOT modify the content of entrypoint.sh
+# Use ./config/static_tests/pre-entrypoint-helpers.sh or ./config/static_tests/post-entrypoint-helpers.sh 
+# to load any customizations or additional configurations
 
 ## NOTE: paths may differ when running in a managed task. To ensure behavior is consistent between
 # managed and local tasks always use these variables for the project and project type path
 PROJECT_PATH=${BASE_PATH}/project
 PROJECT_TYPE_PATH=${BASE_PATH}/projecttype
 
-echo "Starting Static Tests"
+#********** helper functions *************
+pre_entrypoint() {
+    if [ -f ${PROJECT_PATH}/.config/static_tests/pre-entrypoint-helpers.sh ]; then
+        echo "Pre-entrypoint helper found"
+        source ${PROJECT_PATH}/.config/static_tests/pre-entrypoint-helpers.sh
+        echo "Pre-entrypoint helper loaded"
+    else
+        echo "Pre-entrypoint helper not found - skipped"
+    fi
+}
+post_entrypoint() {
+    if [ -f ${PROJECT_PATH}/.config/static_tests/post-entrypoint-helpers.sh ]; then
+        echo "Post-entrypoint helper found"
+        source ${PROJECT_PATH}/.config/static_tests/post-entrypoint-helpers.sh        
+        echo "Post-entrypoint helper loaded"
+    else
+        echo "Post-entrypoint helper not found - skipped"
+    fi
+}
 
-cd ${PROJECT_PATH}
-terraform init
-terraform validate
+#********** Pre-entrypoint helper *************
+pre_entrypoint
 
-#********** tflint ********************
-echo 'Starting tflint'
-tflint --init --config ${PROJECT_PATH}/.config/.tflint.hcl
-MYLINT=$(tflint --force --config ${PROJECT_PATH}/.config/.tflint.hcl)
-if [ -z "$MYLINT" ]
+#********** Static Test *************
+/bin/bash ${PROJECT_PATH}/.project_automation/static_tests/static_tests.sh
+if [ $? -eq 0 ]
 then
-    echo "Success - tflint found no linting issues!"
+    echo "Static test completed"
+    EXIT_CODE=0
 else
-    echo "Failure - tflint found linting issues!"
-    echo "$MYLINT"
-    exit 1
-fi
-#********** tfsec *********************
-echo 'Starting tfsec'
-MYTFSEC=$(tfsec . --config-file ${PROJECT_PATH}/.config/tfsec.yml || true)
-if [[ $MYTFSEC == *"No problems detected!"* ]];
-then
-    echo "Success - tfsec found no security issues!"
-    echo "$MYTFSEC"
-else
-    echo "Failure - tfsec found security issues!"
-    echo "$MYTFSEC"
-    exit 1
+    echo "Static test failed"
+    EXIT_CODE=1
 fi
 
-#********** Markdown Lint **************
-echo 'Starting markdown lint'
-MYMDL=$(mdl --config ${PROJECT_PATH}/.config/.mdlrc .header.md || true)
-if [ -z "$MYMDL" ]
-then
-    echo "Success - markdown lint found no linting issues!"
-else
-    echo "Failure - markdown lint found linting issues!"
-    echo "$MYMDL"
-    exit 1
-fi
-#********** Terraform Docs *************
-echo 'Starting terraform-docs'
-TDOCS="$(terraform-docs --config ${PROJECT_PATH}/.config/.terraform-docs.yaml --lockfile=false ./)"
-git add -N README.md
-GDIFF="$(git diff --compact-summary)"
-if [ -z "$GDIFF" ]
-then
-    echo "Success - Terraform Docs creation verified!"
-else
-    echo "Failure - Terraform Docs creation failed, ensure you have precommit installed and running before submitting the Pull Request"
-    exit 1
-fi
-#***************************************
-echo "End of Static Tests"
+#********** Post-entrypoint helper *************
+post_entrypoint
+
+#********** Exit Code *************
+exit $EXIT_CODE
