ce1a0d12a4082e1a9fad77707b6e1e20794f565f
hacker65536 <s.hacker65536@gmail.com>
2024-04-09 15:17:15 +0900

    fix

--- main.tf
+++ main.tf
@@ -201,7 +201,7 @@ resource "aws_ssoadmin_account_assignment" "account_assignment" {
   instance_arn       = local.ssoadmin_instance_arn
   permission_set_arn = data.aws_ssoadmin_permission_set.existing_permission_sets[each.value.permission_set].arn
 
-  principal_id   = each.value.principal_type == "GROUP" ? data.aws_identitystore_group.identity_store_group[each.value.principal_name].id : data.aws_identitystore_user.identity_store_user[each.value.principal_name].id
+  principal_id   = each.value.principal_type == "GROUP" ? (aws_identitystore_group.sso_groups[each.value.principal_name].group_id != null ? aws_identitystore_group.sso_groups[each.value.principal_name].group_id : data.aws_identitystore_group.identity_store_group[each.value.principal_name].id) : (aws_identitystore_user.sso_users[each.value.principal_name].user_id != null ? aws_identitystore_user.sso_users[each.value.principal_name].user_id : data.aws_identitystore_user.identity_store_user[each.value.principal_name].id)
   principal_type = each.value.principal_type
 
   target_id   = each.value.account_id
