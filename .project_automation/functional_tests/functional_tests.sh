8e91fc310755b7709d926cb0dffcc05ec3f4fdec
IA Automator <dev@null>
2024-04-09 03:58:26 +0000

    (automated) Updates from project type

---
+++ .project_automation/functional_tests/functional_tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## NOTE: paths may differ when running in a managed task. To ensure behavior is consistent between
+# managed and local tasks always use these variables for the project and project type path
+PROJECT_PATH=${BASE_PATH}/project
+PROJECT_TYPE_PATH=${BASE_PATH}/projecttype
+
+echo "Starting Functional Tests"
+
+#********** Terratest execution **********
+cd ${PROJECT_PATH}
+echo "Running Terratest"
+cd test
+rm -f go.mod
+go mod init github.com/aws-ia/terraform-project-ephemeral
+go mod tidy
+go install github.com/gruntwork-io/terratest/modules/terraform
+go test -timeout 45m
+
+echo "End of Functional Tests"
