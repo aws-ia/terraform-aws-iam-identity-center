a6e9e40bf743888e14df0f014f389d1208b85777
hacker65536 <s.hacker65536@gmail.com>
2024-05-20 19:27:09 +0900

    update aws provider version

--- providers.tf
+++ providers.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.35.0"
+      version = ">= 5.46.0"
     }
     awscc = {
       source  = "hashicorp/awscc"
