2dfef4ac1d66a48fafe6882cc0068496d42346b5
Kevon Mayers <novekm@amazon.com>
2024-06-25 20:35:32 -0400

    update pipeline config

--- .project_automation/functional_tests/entrypoint.sh
+++ .project_automation/functional_tests/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## WARNING: DO NOT modify the content of entrypoint.sh
-# Use ./config/functional_tests/pre-entrypoint-helpers.sh or ./config/functional_tests/post-entrypoint-helpers.sh 
+# Use ./config/functional_tests/pre-entrypoint-helpers.sh or ./config/functional_tests/post-entrypoint-helpers.sh
 # to load any customizations or additional configurations
 
 ## NOTE: paths may differ when running in a managed task. To ensure behavior is consistent between
@@ -22,7 +22,7 @@ pre_entrypoint() {
 post_entrypoint() {
     if [ -f ${PROJECT_PATH}/.config/functional_tests/post-entrypoint-helpers.sh ]; then
         echo "Post-entrypoint helper found"
-        source ${PROJECT_PATH}/.config/functional_tests/post-entrypoint-helpers.sh        
+        source ${PROJECT_PATH}/.config/functional_tests/post-entrypoint-helpers.sh
         echo "Post-entrypoint helper loaded"
     else
         echo "Post-entrypoint helper not found - skipped"
@@ -32,33 +32,6 @@ post_entrypoint() {
 #********** Pre-entrypoint helper *************
 pre_entrypoint
 
-<<<<<<< before updating
-#********** TF Env Vars *************
-export AWS_DEFAULT_REGION=us-east-1
-
-
-#********** Checkov Analysis *************
-echo "Running Checkov Analysis"
-terraform init
-terraform plan -out tf.plan
-terraform show -json tf.plan  > tf.json
-checkov --config-file ${PROJECT_PATH}/.config/.checkov.yml
-
-
-#********** Terratest execution **********
-echo "Running Terratest"
-export GOPROXY=https://goproxy.io,direct
-cd test
-rm -f go.mod
-go mod init github.com/aws-ia/terraform-project-ephemeral
-go mod tidy
-go install github.com/gruntwork-io/terratest/modules/terraform
-go test -timeout 45m
-
-echo "End of Functional Tests"
-
-
-=======
 #********** Functional Test *************
 /bin/bash ${PROJECT_PATH}/.project_automation/functional_tests/functional_tests.sh
 if [ $? -eq 0 ]
@@ -75,4 +48,3 @@ post_entrypoint
 
 #********** Exit Code *************
 exit $EXIT_CODE
->>>>>>> after updating
