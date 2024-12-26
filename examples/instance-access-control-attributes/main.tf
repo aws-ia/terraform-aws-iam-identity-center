a876a4f29988f7fa043c6e925ceb0f2c1675baad
David Tappert <tap.developer@outlook.com>
2024-12-26 16:49:43 +0100

    Adding the missing comments that provide hints as to why your test case is failing - we should really look at a better way to do these tests.

--- examples/instance-access-control-attributes/main.tf
+++ examples/instance-access-control-attributes/main.tf
@@ -1,7 +1,7 @@
 module "aws-iam-identity-center" {
     source = "../.." // local example
 
-
+  //Create desired access control attributes
   sso_instance_access_control_attributes = {
     FirstName = { 
       attribute_name = "FirstName"
